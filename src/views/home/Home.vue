aa476d4b2dcfc0435b63daeba8c61c29c82ebd71
zhanglibo <zhanglibo@126.com>
2020-07-21 18:12:42 +0800

    tabbar配置路由映射关系

---
+++ src/views/home/Home.vue
@@ -0,0 +1,17 @@
+<!--  -->
+<template>
+  <div id="home">
+  </div>
+</template>
+
+<script>
+
+export default {
+};
+</script>
+<style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
+</style>
