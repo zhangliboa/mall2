9012bcc77815d8e7244da1a2520273138973c043
zhanglibo <zhanglibo@126.com>
2020-07-22 11:29:00 +0800

    navbar封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,18 @@
 <!--  -->
 <template>
   <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
   </div>
 </template>
 
 <script>
-
+import NavBar from "components/common/navbar/NavBar";
 export default {
+  components: {
+    NavBar
+  }
 };
 </script>
 <style scoped>
